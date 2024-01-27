e3625df518bf8aad77f6c1b297eda55b9dab952d
Sina Moradi <Moradix96@gmail.com>
2024-01-27 17:08:56 +0330

    Init Commit (Added OpenStreetMap Dependency and required permissions)

---
+++ app/src/main/java/ir/co/holoo/sinamap/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.co.holoo.sinamap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
