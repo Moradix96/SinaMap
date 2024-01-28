1c6397ab7f21adab7c1e651c4147f27bcc497fe7
Sina Moradi <Moradix96@gmail.com>
2024-01-28 08:58:21 +0330

    Few changes in go current location, Added marker

--- app/src/main/java/ir/co/holoo/sinamap/MainActivity.kt
+++ app/src/main/java/ir/co/holoo/sinamap/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -20,6 +21,10 @@ import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus
+import org.osmdroid.views.overlay.OverlayItem
 
 
 class MainActivity : AppCompatActivity() {
@@ -66,31 +71,56 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         binding.btnGoMyLocation.setOnClickListener {
+            Log.d("TAG", "btnGoMyLocation Clicked")
 
             if (ContextCompat.checkSelfPermission(
                     this,
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 ) == PackageManager.PERMISSION_GRANTED
             ) {
-                locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                Log.d("TAG", "PERMISSION_WAS_GRANTED")
+
 
                 val location =
                     locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
                 val latitude = location?.latitude
                 val longitude = location?.longitude
-
-                Toast.makeText(
-                    this,
-                    (latitude.toString() + ", " + longitude.toString()),
-                    Toast.LENGTH_SHORT
-                ).show()
+                Log.d("TAG", "Goto " + latitude + "," + longitude)
+                Toast.makeText(this, "اینجا: " + latitude + "," + longitude, Toast.LENGTH_SHORT)
+                    .show()
+                if (latitude != null && longitude != null) {
+                    val myPoint1 = GeoPoint(latitude, longitude)
+                    addMarker(
+                        map,
+                        latitude,
+                        longitude,
+                        R.drawable.map_pin_svgrepo_com,
+                        "موقعیت کنونی شما"
+                    )
+                    mapController.setCenter(myPoint1)
+                    mapController.setZoom(19.0)
+                    binding.map.controller.animateTo(myPoint1)
+                } else {
+                    Log.d("TAG", "Current location is null")
+                    Toast.makeText(
+                        this,
+                        "مشکلی هنگام دریافت موقعیت کنونی شما رخ داد!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             } else {
+                Log.e("TAG", "Permission Denied")
+
                 // Permission is not granted. Request for permission.
                 ActivityCompat.requestPermissions(
                     this,
-                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
                     123456
                 )
             }
@@ -108,20 +138,66 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 123456) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
                 val location =
                     locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
                 val latitude = location?.latitude
                 val longitude = location?.longitude
+                Log.d("TAG", "Goto " + latitude + "," + longitude)
+                Toast.makeText(this, "اینجا: " + latitude + "," + longitude, Toast.LENGTH_SHORT)
+                    .show()
+                if (latitude != null && longitude != null) {
+                    val myPoint1 = GeoPoint(latitude, longitude)
+                    binding.map.controller.setCenter(myPoint1)
+                    binding.map.controller.setZoom(19.0)
+                    binding.map.controller.animateTo(myPoint1)
+                } else {
+                    Log.d("TAG", "Current location is null")
+                    Toast.makeText(
+                        this,
+                        "مشکلی هنگام دریافت موقعیت کنونی شما رخ داد!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
 
-                Toast.makeText(
-                    this,
-                    (latitude.toString() + ", " + longitude.toString()),
-                    Toast.LENGTH_SHORT
-                ).show()
+
             }
         }
+    }
+
+    fun addMarker(map: MapView, lat: Double, lon: Double, resourceId: Int, toastMessage: String) {
+        // Set the map's center to your location
+        val mapController = map.controller
+        val currentLocation = GeoPoint(lat, lon) // replace lat, lon with your coordinates
+        mapController.setCenter(currentLocation)
+
+        // Create an OverlayItem to mark your location
+        val myLocationOverlayItem = OverlayItem("اینجا", "موقعیت کنونی", currentLocation)
+
+        // Set a custom marker if you want (optional)
+        val markerDrawable = ContextCompat.getDrawable(applicationContext, resourceId)
+        myLocationOverlayItem.setMarker(markerDrawable)
+
+        // Create an ItemizedOverlayWithFocus and add your OverlayItem
+        val items = ArrayList<OverlayItem>()
+        items.add(myLocationOverlayItem)
+        val markersOverlay = ItemizedOverlayWithFocus(items,
+            object : ItemizedIconOverlay.OnItemGestureListener<OverlayItem> {
+                override fun onItemSingleTapUp(index: Int, item: OverlayItem): Boolean {
+                    // Your action here on marker tap
+                    Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT).show()
+                    return true
+                }
+
+                override fun onItemLongPress(index: Int, item: OverlayItem): Boolean {
+                    // Your action here on marker long press
+                    return false
+                }
+            }, applicationContext
+        )
+
+        // Add the overlay to the MapView
+        map.overlays.add(markersOverlay)
     }
 
 
